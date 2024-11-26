86b11d782f0b423fb4f70cba568e1deb59bb3dc6
Basharathahmed <basharathahmed0557@gmail.com>
2024-11-26 16:15:08 +0530

    sa

--- BankTransactions/obj/Debug/net6.0/BankTransactions.AssemblyInfo.cs
+++ BankTransactions/obj/Debug/net6.0/BankTransactions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankTransactions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dfc6b377237d5773c7c9bc381da359dbb61a3c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820f7d4fc931b0c5c57a2409a61b799f76884ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankTransactions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankTransactions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
